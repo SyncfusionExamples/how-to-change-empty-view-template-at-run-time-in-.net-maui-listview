2ebada6dabbbbc8e44e3f70b0488ac08166e487c
JayashreeRavishankar <97932323+JayashreeRavishankar@users.noreply.github.com>
2022-11-26 11:04:48 +0530

    Add files via upload

---
+++ EmptyViewTemplateDemo/EmptyViewTemplateDemo/Helper/EmptyViewDataTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmptyViewTemplateDemo
+{
+    public class EmptyViewDataTemplateSelector : Microsoft.Maui.Controls.DataTemplateSelector
+    {
+        public DataTemplate BasicTemplate { get; set; }
+        public DataTemplate AdvancedTemplate { get; set; }
+
+        public EmptyViewDataTemplateSelector()
+        {
+         
+        }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if(item.ToString().Count() > 10)
+                return AdvancedTemplate;
+            else
+                return BasicTemplate;
+        }
+    }
+}
